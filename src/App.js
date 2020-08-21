45ec9f7b7275e10a6e9ac8b2c23fb15ea3d215fb
John Darke <johndarke@live.co.uk>
2020-08-21 11:11:12 +0100

    break homepage into components, style menu components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './Components/HomePage/homepage.jsx'
+import HomePage from './Pages/HomePage/homepage.jsx'
 
 function App() {
   return (
