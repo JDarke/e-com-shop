c85a80ea36f15bf24d4ea08517cd6642db255fce
John Darke <johndarke@live.co.uk>
2020-08-21 10:15:41 +0100

    init CRA and homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './Components/HomePage/homepage.jsx'
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
