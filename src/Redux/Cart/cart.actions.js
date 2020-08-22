c90c0e2594098a14025e29aae1ee28369b5df7a8
John Darke <johndarke@live.co.uk>
2020-08-22 15:57:22 +0100

    updated shop data

--- src/Redux/Cart/cart.actions.js
+++ src/Redux/Cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartActionTypes from './cart.types.js';
 export const toggleCartDropdown = () => ({
     type: CartActionTypes.TOGGLE_CART_DROPDOWN,
 });
+
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
